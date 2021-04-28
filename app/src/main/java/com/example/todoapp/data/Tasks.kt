cfa9e2e3ab103e0341386263aadd3a5d631bce58
Anuj <anujrai04082002.0@gmail.com>
2021-04-28 14:03:14 +0530

    Added the Layout and Enitity class in App Status:Unknow

---
+++ app/src/main/java/com/example/todoapp/data/Tasks.kt
@@ -0,0 +1,23 @@
+package com.example.todoapp.data
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+import java.text.DateFormat
+
+
+@Parcelize
+@Entity(tableName = "Task_Table")
+data class Tasks(
+        @PrimaryKey(autoGenerate = true)
+        val id: Int = 0,
+        val name: String,
+        val important: Boolean = false,
+        val complete: Boolean = false,
+        val created: Long = System.currentTimeMillis()
+) : Parcelable {
+
+    val createdCurrentTimeData: String
+        get() = DateFormat.getDateTimeInstance().format(created)
+}
