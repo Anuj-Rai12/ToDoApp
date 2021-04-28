ecbb9db23740f9402369bde0a271606d58ebf07f
Anuj <anujrai04082002.0@gmail.com>
2021-04-28 14:53:56 +0530

    Added Navigation Graph in App Status:Successfully

---
+++ app/src/main/java/com/example/todoapp/ui/tasks/TaskEditAddFragment.kt
@@ -0,0 +1,12 @@
+package com.example.todoapp.ui.tasks
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.todoapp.R
+
+class TaskEditAddFragment :Fragment(R.layout.fragment_add_edit_task){
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/todoapp/ui/tasks/TaskFragment.kt
@@ -0,0 +1,12 @@
+package com.example.todoapp.ui.tasks
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import com.example.todoapp.R
+
+class TaskFragment:Fragment(R.layout.fragment_task) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
